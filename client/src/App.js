30762f129bfc98e9af2964294ddbb380cde73227
Parvez2118 <parvezkhan5347@gmail.com>
2024-11-24 13:08:12 +0530

    created server folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const createTask= ()=>{
+    console.log("task");
+  }
   return (
     <div className="App">
+      <input type="text" placeholder='Enter Task...'></input>
+      <button onClick={()=>createTask()}>Create</button>
     <h1>hello</h1>
     </div>
   );
